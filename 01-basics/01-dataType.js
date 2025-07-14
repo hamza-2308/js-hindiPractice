320fab44e6090563ffde918e8eb161f28b28781d
Hamza <mamirhamza2308@gmail.com>
2025-07-14 10:11:36 +0000

    dataType

---
+++ 01-basics/01-dataType.js
@@ -0,0 +1,21 @@
+"use strict "; //treate all js code  as newer version
+//alert() we are using nodejs ,not browser
+
+console.log(23+7);
+console.log("hamzuuu")
+let name="hamzaaa"
+let age=20
+let isLoggedIn=true
+let state=null
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object 
+console.log(typeof undefined )//undefined
+console.log(typeof null )//object
+ 
